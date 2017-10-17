43d0431562809830a76b54c3515b2e4e2f0027bc
subharoop <subharoop124@gmail.com>
2017-10-17 08:53:44 +0530

    added python  code

---
+++ power of number.py
@@ -0,0 +1,4 @@
+a=int(input("Enter the number:")) # int function use for input 
+b=int(input("Enter the power:"))
+c=a**b                           #'**' function we can compute power of number "a**b" means (a to the power b)
+print(c)
