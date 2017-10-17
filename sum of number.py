59d53c410043a60796df35f55bbb931cd7ec3462
subharoop <subharoop124@gmail.com>
2017-10-17 08:48:16 +0530

    added python code

---
+++ sum of number.py
@@ -0,0 +1,5 @@
+a=int(input("Enter First Number:")) # int function use for take input
+b=int(input("Enter Second Number:"))
+c=a+b                                # '+' operator use for addtion
+print("The sum is= ",c)               # print function  use for print something
+                                     # '#' use to comments some thing in python
